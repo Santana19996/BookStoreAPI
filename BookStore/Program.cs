5cc87bb605861ecafa196c5b6ede068fbd23e7ee
Oscar Santana <78528593+Santana19996@users.noreply.github.com>
2024-12-24 08:17:05 +0300

    Models created

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,37 +1,46 @@
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get the connection string from appsettings.json
+var connString = builder.Configuration.GetConnectionString("BookStoreAppDBconnection");
 
-// Add services to the container.
+// Configure services
+builder.Services.AddDbContext<BookStoreContext>(options =>
+    options.UseSqlServer(connString)); // Add DbContext with SQL Server connection
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers(); // Add support for controllers
+
+// Add Swagger/OpenAPI services
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure Serilog for logging
 builder.Host.UseSerilog((ctx, lc) =>
     lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+// Add CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
-
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI();
-    app.UseSwagger();
+    app.UseSwagger();       // Enable Swagger middleware
+    app.UseSwaggerUI();     // Enable Swagger UI
 }
 
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+app.UseCors("AllowAll");    // Apply CORS policy
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers();       // Map controller routes
 
 app.Run();
--- BookStore/obj/Debug/net9.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net9.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61602805c186eaf4f92c0503da8d85aba8721c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
